a2fef742c4bf187dd2d3287298d114ac284c945a
slashformotion <slashformotion@protonmail.com>
2022-08-25 15:05:56 +0200

    Add e2e docker based workflow

--- badaas.go
+++ badaas.go
@@ -14,7 +14,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler: router,
-		Addr:    "127.0.0.1:8000",
+		Addr:    ":8000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
