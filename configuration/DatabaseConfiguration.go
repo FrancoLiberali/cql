156c0f2ed4cad98b9a9f0122e9c23183b12bd744
Théophile Roos <slashformotion@protonmail.com>
2022-10-19 16:40:31 +0200

    Add DatabaseConfiguration interface, impl

--- configuration/DatabaseConfiguration.go
+++ configuration/DatabaseConfiguration.go
@@ -1,42 +1,85 @@
 package configuration
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
 
 // Hold the configuration values for the database connection
-type DatabaseConfiguration struct{}
+type DatabaseConfiguration interface {
+	ConfigurationHolder
+	GetPort() int
+	GetHost() string
+	GetDBName() string
+	GetUsername() string
+	GetPassword() string
+	GetSSLMode() string
+}
+
+// Concrete implementation of the DatabaseConfiguration interface
+type databaseConfigurationImpl struct {
+	port     int
+	host     string
+	dbName   string
+	username string
+	password string
+	sslmode  string
+}
 
 // Instantiate a new configuration holder for the database connection
-func NewDatabaseConfiguration() *DatabaseConfiguration {
-	return &DatabaseConfiguration{}
+func NewDatabaseConfiguration() DatabaseConfiguration {
+	databaseConfiguration := new(databaseConfigurationImpl)
+	databaseConfiguration.Reload()
+	return databaseConfiguration
+}
+
+// Reload database configuration
+func (databaseConfiguration *databaseConfigurationImpl) Reload() {
+	databaseConfiguration.port = viper.GetInt("database.port")
+	databaseConfiguration.host = viper.GetString("database.host")
+	databaseConfiguration.dbName = viper.GetString("database.name")
+	databaseConfiguration.username = viper.GetString("database.username")
+	databaseConfiguration.password = viper.GetString("database.password")
+	databaseConfiguration.sslmode = viper.GetString("database.sslmode")
 }
 
 // Return the port of the database server
-func (dbc *DatabaseConfiguration) GetPort() int {
-	return viper.GetInt("database.port")
+func (databaseConfiguration *databaseConfigurationImpl) GetPort() int {
+	return databaseConfiguration.port
 }
 
 // Return the host of the database server
-func (dbc *DatabaseConfiguration) GetHost() string {
-	return viper.GetString("database.host")
+func (databaseConfiguration *databaseConfigurationImpl) GetHost() string {
+	return databaseConfiguration.host
 }
 
 // Return the database name
-func (dbc *DatabaseConfiguration) GetDBName() string {
-	return viper.GetString("database.name")
+func (databaseConfiguration *databaseConfigurationImpl) GetDBName() string {
+	return databaseConfiguration.dbName
 }
 
 // Return the username of the user on the database server
-func (dbc *DatabaseConfiguration) GetUsername() string {
-	return viper.GetString("database.username")
+func (databaseConfiguration *databaseConfigurationImpl) GetUsername() string {
+	return databaseConfiguration.username
 }
 
 // Return the password of the user on the database server
-func (dbc *DatabaseConfiguration) GetPassword() string {
-	return viper.GetString("database.password")
-
+func (databaseConfiguration *databaseConfigurationImpl) GetPassword() string {
+	return databaseConfiguration.password
 }
 
 // Return the sslmode for the database connection
-func (dbc *DatabaseConfiguration) GetSSLMode() string {
-	return viper.GetString("database.sslmode")
+func (databaseConfiguration *databaseConfigurationImpl) GetSSLMode() string {
+	return databaseConfiguration.sslmode
+}
+
+func (databaseConfiguration *databaseConfigurationImpl) Log() {
+	zap.L().Info("Database configuration",
+		zap.Int("port", databaseConfiguration.port),
+		zap.String("host", databaseConfiguration.host),
+		zap.String("dbName", databaseConfiguration.dbName),
+		zap.String("username", databaseConfiguration.username),
+		zap.String("password", databaseConfiguration.password),
+		zap.String("sslmode", databaseConfiguration.sslmode),
+	)
 }
