85e821a9f5a29f365901d27d68db9225cd09b7aa
Théophile Roos <slashformotion@protonmail.com>
2022-10-19 16:41:26 +0200

    Add PaginationConfiguration interface, impl

--- configuration/PaginationConfiguration.go
+++ configuration/PaginationConfiguration.go
@@ -1,20 +1,40 @@
 package configuration
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
 
+// Hold the configuration values for the pagination
 type PaginationConfiguration interface {
+	ConfigurationHolder
 	GetMaxElemPerPage() uint
 }
 
-// Hold the configuration values for the pagination
-type paginationConfigurationImpl struct{}
+// Concrete implementation of the PaginationConfiguration interface
+type paginationConfigurationImpl struct {
+	pagesNb uint
+}
 
 // Instantiate a new configuration holder for the pagination
 func NewPaginationConfiguration() PaginationConfiguration {
-	return &paginationConfigurationImpl{}
+	paginationConfiguration := new(paginationConfigurationImpl)
+	paginationConfiguration.Reload()
+	return paginationConfiguration
 }
 
 // Return the maximum number of element returned per page
-func (lc *paginationConfigurationImpl) GetMaxElemPerPage() uint {
-	return viper.GetUint("server.pagination.page.max")
+func (paginationConfiguration *paginationConfigurationImpl) GetMaxElemPerPage() uint {
+	return paginationConfiguration.pagesNb
+}
+
+func (paginationConfiguration *paginationConfigurationImpl) Log() {
+	zap.L().Info("Pagination configuration",
+		zap.Uint("maxelemPerPage", paginationConfiguration.pagesNb),
+	)
+}
+
+// Reload pagination configuration
+func (paginationConfiguration *paginationConfigurationImpl) Reload() {
+	paginationConfiguration.pagesNb = viper.GetUint("server.pagination.page.max")
 }
