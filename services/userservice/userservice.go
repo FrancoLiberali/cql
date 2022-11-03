85597a811f73cd1d0b1d5622790b4362a40ed1e4
Théophile Roos <slashformotion@protonmail.com>
2022-11-03 16:30:00 +0100

    Add refactored user service

--- services/userservice/userservice.go
+++ services/userservice/userservice.go
@@ -4,20 +4,54 @@ import (
 	"fmt"
 
 	"github.com/ditrit/badaas/persistence/models"
-	"github.com/ditrit/badaas/persistence/registry"
+	"github.com/ditrit/badaas/persistence/repository"
 	"github.com/ditrit/badaas/services/auth/protocols/basicauth"
 	validator "github.com/ditrit/badaas/validators"
+	"go.uber.org/zap"
 )
 
+// UserService provide functions related to Users
+type UserService interface {
+	NewUser(username, email, password string) (*models.User, error)
+}
+
+// Check interface compliance
+var _ UserService = (*userServiceImpl)(nil)
+
+// The UserService concrete implementation
+type userServiceImpl struct {
+	userRepository repository.CRUDRepository[models.User]
+	logger         *zap.Logger
+}
+
+// UserService constructor
+func NewUserService(
+	logger *zap.Logger,
+	userRepository repository.CRUDRepository[models.User],
+) UserService {
+	return &userServiceImpl{
+		logger:         logger,
+		userRepository: userRepository,
+	}
+}
+
 // Create a new user
-func NewUser(username, email, password string) (*models.User, error) {
-	if !validator.ValidEmail(email) {
+func (userService *userServiceImpl) NewUser(username, email, password string) (*models.User, error) {
+	sanitizedEmail, err := validator.ValidEmail(email)
+	if err != nil {
 		return nil, fmt.Errorf("the provided email is not valid")
 	}
 	u := &models.User{
 		Username: username,
-		Email:    email,
+		Email:    sanitizedEmail,
 		Password: basicauth.SaltAndHashPassword(password),
 	}
-	return u, registry.GetRegistry().UserRepository.Create(u)
+	httpError := userService.userRepository.Create(u)
+	if httpError != nil {
+		return nil, httpError
+	}
+	userService.logger.Info("Successfully created a new user",
+		zap.String("email", sanitizedEmail), zap.String("username", username))
+
+	return u, nil
 }
