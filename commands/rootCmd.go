27ff65cb9353573c2415c6654732871fdfe2ec6c
Théophile Roos <slashformotion@protonmail.com>
2022-11-16 12:15:10 +0100

    Add init function

--- commands/rootCmd.go
+++ commands/rootCmd.go
@@ -2,12 +2,14 @@ package commands
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/ditrit/badaas/configuration"
 	"github.com/ditrit/badaas/controllers"
 	"github.com/ditrit/badaas/logger"
 	"github.com/ditrit/badaas/persistence"
 	"github.com/ditrit/badaas/router"
+	"github.com/ditrit/badaas/services/userservice"
 	"github.com/ditrit/verdeter"
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxevent"
@@ -24,26 +26,40 @@ func runHTTPServer(cfg *verdeter.VerdeterCommand, args []string) error {
 		logger.LoggerModule,
 		persistence.PersistanceModule,
 
+		fx.Provide(userservice.NewUserService),
 		// logger for fx
-		fx.Provide(fxLogger),
+		fx.WithLogger(func(logger *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: logger}
+		}),
 
 		fx.Provide(NewHTTPServer),
 
 		// Finally: we invoke the newly created server
 		fx.Invoke(func(*http.Server) { /* we need this function to be empty*/ }),
+		fx.Invoke(createSuperUser),
 	).Run()
 	return nil
 }
 
-func fxLogger(logger *zap.Logger, loggerConfiguration configuration.LoggerConfiguration) fx.Option {
-	if loggerConfiguration.GetMode() == "dev" {
-		return fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
-			return &fxevent.ZapLogger{Logger: log}
-		})
+// create a super user
+func createSuperUser(
+	config configuration.InitializationConfiguration,
+	logger *zap.Logger,
+	userService userservice.UserService,
+) error {
+	// Create a super admin user and exit with code 1 on error
+	_, err := userService.NewUser("admin", "admin-no-reply@badaas.com", config.GetAdminPassword())
+	if err != nil {
+		if !strings.Contains(err.Error(), "already exist in database") {
+			logger.Sugar().Fatalf("failed to save the super admin %w", err)
+			return err
+		}
+		logger.Sugar().Infof("The superadmin user already exists in database")
 	}
-	return fx.NopLogger
+	return nil
 }
 
+// The command badaas
 var rootCfg = verdeter.NewVerdeterCommand(
 	"badaas",
 	"Backend and Distribution as a Service",
@@ -64,4 +80,5 @@ func init() {
 	initServerCommands(rootCfg)
 	initLoggerCommands(rootCfg)
 	initDatabaseCommands(rootCfg)
+	initInitialisationCommands(rootCfg)
 }
