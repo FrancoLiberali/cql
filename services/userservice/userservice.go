4450ce4e32f75a499b24201f5148c462e963992e
Théophile Roos <slashformotion@protonmail.com>
2022-12-19 12:08:10 +0100

    Update user service

--- services/userservice/userservice.go
+++ services/userservice/userservice.go
@@ -3,7 +3,10 @@ package userservice
 import (
 	"fmt"
 
+	"github.com/Masterminds/squirrel"
+	"github.com/ditrit/badaas/httperrors"
 	"github.com/ditrit/badaas/persistence/models"
+	"github.com/ditrit/badaas/persistence/models/dto"
 	"github.com/ditrit/badaas/persistence/repository"
 	"github.com/ditrit/badaas/services/auth/protocols/basicauth"
 	validator "github.com/ditrit/badaas/validators"
@@ -13,6 +16,7 @@ import (
 // UserService provide functions related to Users
 type UserService interface {
 	NewUser(username, email, password string) (*models.User, error)
+	GetUser(dto.UserLoginDTO) (*models.User, httperrors.HTTPError)
 }
 
 // Check interface compliance
@@ -54,4 +58,24 @@ func (userService *userServiceImpl) NewUser(username, email, password string) (*
 		zap.String("email", sanitizedEmail), zap.String("username", username))
 
 	return u, nil
+}
+
+// Get user if the email and password provided are correct, return an error if not. 
+func (userService *userServiceImpl) GetUser(userLoginDTO dto.UserLoginDTO) (*models.User, httperrors.HTTPError) {
+	users, herr := userService.userRepository.Find(squirrel.Eq{"email": userLoginDTO.Email}, nil, nil)
+	if herr != nil {
+		return nil, herr
+	}
+	if !users.HasContent {
+		return nil, httperrors.NewErrorNotFound("user",
+			fmt.Sprintf("no user found with email %q", userLoginDTO.Email))
+	}
+
+	user := users.Ressources[0]
+
+	// Check password
+	if !basicauth.CheckUserPassword(user.Password, userLoginDTO.Password) {
+		return nil, httperrors.NewUnauthorizedError("wrong password", "the provided password is incorrect")
+	}
+	return user, nil
 }
--- services/userservice/userservice_test.go
+++ services/userservice/userservice_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/ditrit/badaas/httperrors"
 	repositorymocks "github.com/ditrit/badaas/mocks/persistence/repository"
 	"github.com/ditrit/badaas/persistence/models"
+	"github.com/ditrit/badaas/persistence/models/dto"
+	"github.com/ditrit/badaas/persistence/pagination"
 	"github.com/ditrit/badaas/services/userservice"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -75,4 +77,115 @@ func TestNewUserServiceEmailNotValid(t *testing.T) {
 
 	// Checking logs
 	assert.Equal(t, 0, observedLogs.Len())
+}
+
+func TestGetUser(t *testing.T) {
+	// creating logger
+	observedZapCore, observedLogs := observer.New(zap.DebugLevel)
+	observedLogger := zap.New(observedZapCore)
+
+	userRespositoryMock := repositorymocks.NewCRUDRepository[models.User, uint](t)
+	userService := userservice.NewUserService(observedLogger, userRespositoryMock)
+	userRespositoryMock.On(
+		"Create", mock.Anything,
+	).Return(
+		nil,
+	)
+	user, err := userService.NewUser("bob", "bob@email.com", "1234")
+
+	require.NoError(t, err)
+	userRespositoryMock.On(
+		"Find", mock.Anything, nil, nil,
+	).Return(
+		pagination.NewPage([]*models.User{user}, 1, 10, 50),
+		nil,
+	)
+
+	userFound, err := userService.GetUser(dto.UserLoginDTO{Email: "bob@email.com", Password: "1234"})
+	require.NoError(t, err)
+	assert.Equal(t, user, userFound)
+	// Checking logs
+	assert.Equal(t, 1, observedLogs.Len())
+	log := observedLogs.All()[0]
+	assert.Equal(t, "Successfully created a new user", log.Message)
+	assert.ElementsMatch(t, []zap.Field{
+		{Key: "email", Type: zapcore.StringType, String: "bob@email.com"},
+		{Key: "username", Type: zapcore.StringType, String: "bob"},
+	}, log.Context)
+	assert.Equal(t, zap.InfoLevel, log.Level)
+}
+
+func TestGetUserNoUserFound(t *testing.T) {
+	// creating logger
+	observedZapCore, _ := observer.New(zap.DebugLevel)
+	observedLogger := zap.New(observedZapCore)
+
+	userRespositoryMock := repositorymocks.NewCRUDRepository[models.User, uint](t)
+	userService := userservice.NewUserService(observedLogger, userRespositoryMock)
+	userRespositoryMock.On(
+		"Find", mock.Anything, nil, nil,
+	).Return(
+		nil,
+		httperrors.NewErrorNotFound("user", "user with email bobnotfound@email.com"),
+	)
+
+	userFound, err := userService.GetUser(dto.UserLoginDTO{Email: "bobnotfound@email.com", Password: "1234"})
+	require.Error(t, err)
+	assert.Nil(t, userFound)
+}
+
+// Check what happen if the pass word is not correct
+func TestGetUserNotCorrect(t *testing.T) {
+	// creating logger
+	observedZapCore, _ := observer.New(zap.DebugLevel)
+	observedLogger := zap.New(observedZapCore)
+
+	userRespositoryMock := repositorymocks.NewCRUDRepository[models.User, uint](t)
+	userRespositoryMock.On(
+		"Create", mock.Anything,
+	).Return(
+		nil,
+	)
+	userService := userservice.NewUserService(observedLogger, userRespositoryMock)
+	user, err := userService.NewUser("bob", "bob@email.com", "1234")
+
+	require.NoError(t, err)
+	userRespositoryMock.On(
+		"Find", mock.Anything, nil, nil,
+	).Return(
+		pagination.NewPage([]*models.User{user}, 1, 10, 50),
+		nil,
+	)
+
+	userFound, err := userService.GetUser(dto.UserLoginDTO{Email: "bob@email.com", Password: "<sdùfjidsfnjd"})
+	require.Error(t, err)
+	assert.Nil(t, userFound)
+}
+
+// Check what happen if the repository return an empty list
+func TestGetUserEmpty(t *testing.T) {
+	// Creating logger
+	observedZapCore, _ := observer.New(zap.DebugLevel)
+	observedLogger := zap.New(observedZapCore)
+
+	userRespositoryMock := repositorymocks.NewCRUDRepository[models.User, uint](t)
+	userRespositoryMock.On(
+		"Create", mock.Anything,
+	).Return(
+		nil,
+	)
+	userService := userservice.NewUserService(observedLogger, userRespositoryMock)
+	_, err := userService.NewUser("bob", "bob@email.com", "1234")
+
+	require.NoError(t, err)
+	userRespositoryMock.On(
+		"Find", mock.Anything, nil, nil,
+	).Return(
+		pagination.NewPage([]*models.User{}, 1, 10, 50),
+		nil,
+	)
+
+	userFound, err := userService.GetUser(dto.UserLoginDTO{Email: "bob@email.com", Password: "<sdùfjidsfnjd"})
+	require.Error(t, err)
+	assert.Nil(t, userFound)
 }
