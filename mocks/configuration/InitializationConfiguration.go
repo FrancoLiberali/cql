7789ba2333e816c263500fae096e757ce7a36cf7
Théophile Roos <slashformotion@protonmail.com>
2022-12-20 10:24:17 +0100

    add mocks

--- mocks/configuration/InitializationConfiguration.go
+++ mocks/configuration/InitializationConfiguration.go
@@ -12,15 +12,15 @@ type InitializationConfiguration struct {
 	mock.Mock
 }
 
-// GetInit provides a mock function with given fields:
-func (_m *InitializationConfiguration) GetInit() bool {
+// GetAdminPassword provides a mock function with given fields:
+func (_m *InitializationConfiguration) GetAdminPassword() string {
 	ret := _m.Called()
 
-	var r0 bool
-	if rf, ok := ret.Get(0).(func() bool); ok {
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
 		r0 = rf()
 	} else {
-		r0 = ret.Get(0).(bool)
+		r0 = ret.Get(0).(string)
 	}
 
 	return r0
