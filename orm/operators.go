ditrit/badaas

add not between

orm/operators.go
@@ -41,6 +41,11 @@ func Between[T any](v1 T, v2 T) MultivalueOperator[T] {
 	return NewMultivalueOperator("BETWEEN", "AND", "", "", v1, v2)
 }
 
+// Equivalent to NOT (v1 < value < v2)
+func NotBetween[T any](v1 T, v2 T) MultivalueOperator[T] {
+	return NewMultivalueOperator("NOT BETWEEN", "AND", "", "", v1, v2)
+}
+
 func IsNull[T any]() PredicateOperator[T] {
 	return NewPredicateOperator[T]("IS NULL")
 }

FrancoLiberali/cql

orm/operators.go
@@ -43,6 +43,11 @@ func Between[T any](v1 T, v2 T) Operator[T] {
 	return newBetweenOperator("BETWEEN", v1, v2)
 }
 
+// Equivalent to NOT (v1 < value < v2)
+func NotBetween[T any](v1 T, v2 T) Operator[T] {
+	return newBetweenOperator("NOT BETWEEN", v1, v2)
+}
+
 func newBetweenOperator[T any](sqlOperator string, v1 T, v2 T) Operator[T] {
 	operator := NewValueOperator[T](sqlOperator, v1)
 	return operator.AddOperation("AND", v2)