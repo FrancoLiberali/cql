b28139100d530333399c5b934f8a152f79aaa5bf
Franco Liberali <franco.liberali@gmail.com>
2024-01-07 18:18:44 -0300

    delete and update at least one condition compiled (#48)

--- delete.go
+++ delete.go
@@ -7,9 +7,15 @@ import (
 	"github.com/FrancoLiberali/cql/model"
 )
 
-// Create a Delete to which the conditions are applied inside transaction tx
+// Create a Delete to which the conditions are applied inside transaction tx.
+//
+// At least one condition is required to avoid deleting all values in a table.
+// In case this is the desired behavior, use cql.True.
 //
 // For details see https://compiledquerylenguage.readthedocs.io/en/latest/cql/delete.html
-func Delete[T model.Model](tx *gorm.DB, conditions ...condition.Condition[T]) *condition.Delete[T] {
-	return condition.NewDelete(tx, conditions...)
+func Delete[T model.Model](tx *gorm.DB, firstCondition condition.Condition[T], conditions ...condition.Condition[T]) *condition.Delete[T] {
+	return condition.NewDelete(
+		tx,
+		append(conditions, firstCondition)...,
+	)
 }
