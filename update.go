aac14e8dec979acb7b3232b64134d3c1654aee44
Franco Liberali <franco.liberali@gmail.com>
2024-01-07 18:18:44 -0300

    delete and update at least one condition compiled (#48)

--- update.go
+++ update.go
@@ -7,9 +7,15 @@ import (
 	"github.com/FrancoLiberali/cql/model"
 )
 
-// Create a Update to which the conditions are applied inside transaction tx
+// Create a Update to which the conditions are applied inside transaction tx.
+//
+// At least one condition is required to avoid updating all values in a table.
+// In case this is the desired behavior, use cql.True.
 //
 // For details see https://compiledquerylenguage.readthedocs.io/en/latest/cql/update.html
-func Update[T model.Model](tx *gorm.DB, conditions ...condition.Condition[T]) *condition.Update[T] {
-	return condition.NewUpdate(tx, conditions...)
+func Update[T model.Model](tx *gorm.DB, firstCondition condition.Condition[T], conditions ...condition.Condition[T]) *condition.Update[T] {
+	return condition.NewUpdate(
+		tx,
+		append(conditions, firstCondition)...,
+	)
 }
