5feb815c5c90014ea96edf1071fa6aa6f14def4b
FrancoLiberali <franco.liberali@gmail.com>
2024-01-07 23:29:37 -0300

    report in correct line

--- cql-lint/pkg/analyzer/analyzer.go
+++ cql-lint/pkg/analyzer/analyzer.go
@@ -2,6 +2,7 @@ package analyzer
 
 import (
 	"go/ast"
+	"go/token"
 
 	"github.com/elliotchance/pie/v2"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -20,11 +21,18 @@ var Analyzer = &analysis.Analyzer{
 
 var cqlMethods = []string{"Query", "Update", "Delete"}
 
+type Position struct {
+	Number token.Pos
+	Model  string
+}
+
 func run(pass *analysis.Pass) (interface{}, error) {
 	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 	nodeFilter := []ast.Node{
 		(*ast.CallExpr)(nil),
 	}
+
+	positionsToReport := []Position{}
 
 	inspector.Preorder(nodeFilter, func(node ast.Node) {
 		defer func() {
@@ -58,16 +66,21 @@ func run(pass *analysis.Pass) (interface{}, error) {
 			return
 		}
 
-		findErrorIsDynamic(pass, node, []string{}, callExpr.Args[1:]) // first parameters is ignored as it's the db object
-
-		// pass.Reportf(node.Pos(), "printf-like formatting function '%s' should be named '%sf'",
-		// 	funcDecl.Name.Name, funcDecl.Name.Name)
+		positionsToReport, _ = findErrorIsDynamic(positionsToReport, []string{}, callExpr.Args[1:]) // first parameters is ignored as it's the db object
 	})
+
+	for _, position := range positionsToReport {
+		pass.Reportf(
+			position.Number,
+			"%s is not joined by the query",
+			position.Model,
+		)
+	}
 
 	return nil, nil
 }
 
-func findErrorIsDynamic(pass *analysis.Pass, node ast.Node, models []string, conditions []ast.Expr) []string {
+func findErrorIsDynamic(positionsToReport []Position, models []string, conditions []ast.Expr) ([]Position, []string) {
 	addedToModels := false
 
 	for _, condition := range conditions {
@@ -75,47 +88,51 @@ func findErrorIsDynamic(pass *analysis.Pass, node ast.Node, models []string, con
 		conditionCall := condition.(*ast.CallExpr)
 		conditionSelector := conditionCall.Fun.(*ast.SelectorExpr)
 
-		// TODO tener en cuenta el preload
 		joinCondition, isJoinCondition := conditionSelector.X.(*ast.SelectorExpr)
 		if isJoinCondition {
 			addedToModels = true
 			models = append(models, joinCondition.Sel.Name)
 			// TODO que pasa si el join adentro no tiene wheres -> intentar con el nombre de la relacion y sino bueno falso positivo
-			models = findErrorIsDynamic(pass, node, models, conditionCall.Args)
+			// func testJoinedWithJoinedWithoutCondition() {
+			// 	cql.Query[models.Phone](
+			// 		db,
+			// 		conditions.Phone.Brand(),
+			// 		conditions.Phone.Name.IsDynamic().Eq(conditions.Brand.Name.Value()),
+			// 	).Find()
+			// }
+			positionsToReport, models = findErrorIsDynamic(positionsToReport, models, conditionCall.Args)
 		} else {
 			whereCondition, isWhereCondition := conditionSelector.X.(*ast.CallExpr)
 			if isWhereCondition {
 				if !addedToModels {
 					addedToModels = true
-					_, _, modelName := getFieldAndModel(whereCondition)
+					_, modelName, _ := getModel(whereCondition)
 					models = append(models, modelName)
 				}
 
 				if getFieldIsMethodName(whereCondition) == "IsDynamic" {
-					_, modelPkg, isDynamicModelName := getFieldAndModel(conditionCall.Args[0].(*ast.CallExpr))
+					modelPkg, isDynamicModelName, modelPos := getModel(conditionCall.Args[0].(*ast.CallExpr))
 					if !pie.Contains(models, isDynamicModelName) {
-						pass.Reportf(
-							node.Pos(),
-							"%s is not joined by the query",
-							modelPkg+"."+isDynamicModelName,
-						)
+						positionsToReport = append(positionsToReport, Position{
+							Number: modelPos,
+							Model:  modelPkg + "." + isDynamicModelName,
+						})
 					}
 				}
 			}
 		}
 	}
 
-	return models
+	return positionsToReport, models
 }
 
 func getFieldIsMethodName(whereCondition *ast.CallExpr) string {
 	return whereCondition.Fun.(*ast.SelectorExpr).Sel.Name
 }
 
-// Returns the field name, the model package the model name
-func getFieldAndModel(whereCondition *ast.CallExpr) (string, string, string) {
-	field := whereCondition.Fun.(*ast.SelectorExpr).X.(*ast.SelectorExpr)
-	model := field.X.(*ast.SelectorExpr)
+// Returns model's package the model name
+func getModel(whereCondition *ast.CallExpr) (string, string, token.Pos) {
+	model := whereCondition.Fun.(*ast.SelectorExpr).X.(*ast.SelectorExpr).X.(*ast.SelectorExpr)
 
-	return field.Sel.Name, model.X.(*ast.Ident).Name, model.Sel.Name
+	return model.X.(*ast.Ident).Name, model.Sel.Name, model.Pos()
 }
--- cql-lint/pkg/analyzer/testdata/src/a/a.go
+++ cql-lint/pkg/analyzer/testdata/src/a/a.go
@@ -10,11 +10,97 @@ import (
 
 var db *gorm.DB
 
-func testError() {
-	cql.Query[models.Phone]( // want "conditions.City is not joined by the query"
+func testNotJoinedInSameLine() {
+	cql.Query[models.Brand](
+		db,
+		conditions.Brand.Name.IsDynamic().Eq(conditions.City.Name.Value()), // want "conditions.City is not joined by the query"
+	).Find()
+}
+
+func testNotJoinedInDifferentLines() {
+	cql.Query[models.Brand](
+		db,
+		conditions.Brand.Name.IsDynamic().Eq(
+			conditions.City.Name.Value(), // want "conditions.City is not joined by the query"
+		),
+	).Find()
+}
+
+func testNotJoinedInsideJoinCondition() {
+	cql.Query[models.Phone](
 		db,
 		conditions.Phone.Brand(
-			conditions.Brand.Name.IsDynamic().Eq(conditions.City.Name.Value()),
+			conditions.Brand.Name.IsDynamic().Eq(conditions.City.Name.Value()), // want "conditions.City is not joined by the query"
+		),
+	).Find()
+}
+
+func testJoinedWithMainModel() {
+	cql.Query[models.Phone](
+		db,
+		conditions.Phone.Brand(
+			conditions.Brand.Name.IsDynamic().Eq(conditions.Phone.Name.Value()),
+		),
+	).Find()
+}
+
+func testNotJoinedInsideNestedJoinCondition() {
+	cql.Query[models.Child](
+		db,
+		conditions.Child.Parent1(
+			conditions.Parent1.ParentParent(
+				conditions.ParentParent.Name.IsDynamic().Eq(conditions.City.Name.Value()), // want "conditions.City is not joined by the query"
+			),
+		),
+	).Find()
+}
+
+func testJoinedInsideNestedJoinConditionWithMainModel() {
+	cql.Query[models.Child](
+		db,
+		conditions.Child.Parent1(
+			conditions.Parent1.ParentParent(
+				conditions.ParentParent.Name.IsDynamic().Eq(conditions.Child.Name.Value()),
+			),
+		),
+	).Find()
+}
+
+func testJoinedInsideNestedJoinConditionWithPreviousJoin() {
+	cql.Query[models.Child](
+		db,
+		conditions.Child.Parent1(
+			conditions.Parent1.ParentParent(
+				conditions.ParentParent.Name.IsDynamic().Eq(conditions.Parent1.Name.Value()),
+			),
+		),
+	).Find()
+}
+
+func testJoinedWithJoinedWithCondition() {
+	cql.Query[models.Phone](
+		db,
+		conditions.Phone.Brand(
+			conditions.Brand.Name.Is().Eq("asd"),
+		),
+		conditions.Phone.Name.IsDynamic().Eq(conditions.Brand.Name.Value()),
+	).Find()
+}
+
+func testJoinedWithJoinedWithPreload() {
+	cql.Query[models.Phone](
+		db,
+		conditions.Phone.Brand().Preload(),
+		conditions.Phone.Name.IsDynamic().Eq(conditions.Brand.Name.Value()),
+	).Find()
+}
+
+func testNotJoinedWithJoinedWithConditionBefore() {
+	cql.Query[models.Phone](
+		db,
+		conditions.Phone.Name.IsDynamic().Eq(conditions.Brand.Name.Value()), // want "conditions.Brand is not joined by the query"
+		conditions.Phone.Brand(
+			conditions.Brand.Name.Is().Eq("asd"),
 		),
 	).Find()
 }
