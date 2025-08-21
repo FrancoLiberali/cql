848fffa6e893cad27f895b0eb2269e88f8056e34
FrancoLiberali <franco.liberali@gmail.com>
2025-08-20 21:50:27 -0300

    compilation errors for delete and group by

--- test/compilation_test.go
+++ test/compilation_test.go
@@ -11,16 +11,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type testCase struct {
+	Name  string
+	Code  string
+	Error string
+}
+
 func TestQueryCompilationErrors(t *testing.T) {
 	t.Parallel()
 
 	queryMethods := []string{"cql.Query", "cql.Delete", "cql.Update"}
 
-	tests := []struct {
-		Name  string
-		Code  string
-		Error string
-	}{
+	tests := []testCase{
 		{
 			Name: "wrong name of condition",
 			Code: `
@@ -33,207 +35,205 @@ func TestQueryCompilationErrors(t *testing.T) {
 		{
 			Name: "wrong name of property",
 			Code: `
-	_ = %s[models.Product](
-		db,
-		conditions.Product.IntNotExists.Is().Eq(cql.Int(1)),
-	)`,
+		_ = %s[models.Product](
+			db,
+			conditions.Product.IntNotExists.Is().Eq(cql.Int(1)),
+		)`,
 			Error: `conditions.Product.IntNotExists undefined (type conditions.productConditions has no field or method IntNotExists)`,
 		},
 		{
 			Name: "basic wrong type in value",
 			Code: `
-	_ = %s[models.Product](
-		db,
-		conditions.Product.Int.Is().Eq("1"),
-	)`,
+		_ = %s[models.Product](
+			db,
+			conditions.Product.Int.Is().Eq("1"),
+		)`,
 			Error: `cannot use "1" (constant of type string) as condition.ValueOfType[float64] value in argument to conditions.Product.Int.Is().Eq: string does not implement condition.ValueOfType[float64] (missing method GetValue)`,
 		},
 		{
 			Name: "Use wrong type in value",
 			Code: `
-	_ = %s[models.Product](
-		db,
-		conditions.Product.Int.Is().Eq(cql.Int("1")),
-	)`,
+		_ = %s[models.Product](
+			db,
+			conditions.Product.Int.Is().Eq(cql.Int("1")),
+		)`,
 			Error: `cannot use "1" (untyped string constant) as int value in argument to cql.Int`,
 		},
 		{
 			Name: "Compare with wrong type",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Is().Eq(cql.String("1")),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Is().Eq(cql.String("1")),
+			)`,
 			Error: `cannot use cql.String("1") (value of type condition.Value[string]) as condition.ValueOfType[float64] value in argument to conditions.Product.Int.Is().Eq: condition.Value[string] does not implement condition.ValueOfType[float64] (wrong type for method GetValue)`,
 		},
 		{
 			Name: "Compare with wrong type for multiple values operator",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Is().Between(
-				cql.Int(1),
-				cql.String("1"),
-			),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Is().Between(
+					cql.Int(1),
+					cql.String("1"),
+				),
+			)`,
 			Error: `cannot use cql.String("1") (value of type condition.Value[string]) as condition.ValueOfType[float64] value in argument to conditions.Product.Int.Is().Between: condition.Value[string] does not implement condition.ValueOfType[float64] (wrong type for method GetValue)`,
 		},
 		{
 			Name: "Compare with wrong type for list of values operator",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Is().In(
-				cql.Int(1),
-				cql.String("1"),
-			),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Is().In(
+					cql.Int(1),
+					cql.String("1"),
+				),
+			)`,
 			Error: `cannot use cql.String("1") (value of type condition.Value[string]) as condition.ValueOfType[float64] value in argument to conditions.Product.Int.Is().In: condition.Value[string] does not implement condition.ValueOfType[float64] (wrong type for method GetValue)`,
 		},
 		{
 			Name: "Use condition of another model",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Sale.Code.Is().Eq(cql.Int(1)),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Sale.Code.Is().Eq(cql.Int(1)),
+			)`,
 			Error: `cannot use conditions.Sale.Code.Is().Eq(cql.Int(1)) (value of type condition.WhereCondition[models.Sale]) as condition.Condition[models.Product] value in argument to %s[models.Product]: condition.WhereCondition[models.Sale] does not implement condition.Condition[models.Product] (wrong type for method interfaceVerificationMethod)`,
 		},
 		{
 			Name: "Use condition of another model inside join",
 			Code: `
-		_ = %s[models.Sale](
-			db,
-			conditions.Sale.Seller(
-				conditions.Sale.Code.Is().Eq(cql.Int(1)),
-			),
-		)`,
+			_ = %s[models.Sale](
+				db,
+				conditions.Sale.Seller(
+					conditions.Sale.Code.Is().Eq(cql.Int(1)),
+				),
+			)`,
 			Error: `cannot use conditions.Sale.Code.Is().Eq(cql.Int(1)) (value of type condition.WhereCondition[models.Sale]) as condition.Condition[models.Seller] value in argument to conditions.Sale.Seller: condition.WhereCondition[models.Sale] does not implement condition.Condition[models.Seller] (wrong type for method interfaceVerificationMethod)`,
 		},
 		{
 			Name: "Use condition of another model inside logical operator",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			cql.Or(conditions.Sale.Code.Is().Eq(cql.Int(1))),
-		)`,
+			_ = %s[models.Product](
+				db,
+				cql.Or(conditions.Sale.Code.Is().Eq(cql.Int(1))),
+			)`,
 			Error: `cannot use cql.Or(conditions.Sale.Code.Is().Eq(cql.Int(1))) (value of type condition.WhereCondition[models.Sale]) as condition.Condition[models.Product] value in argument to %s[models.Product]: condition.WhereCondition[models.Sale] does not implement condition.Condition[models.Product] (wrong type for method interfaceVerificationMethod)`,
 		},
 		{
 			Name: "Use condition of another model inside logical operator multiple",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			cql.Or[models.Product](
-				conditions.Product.Int.Is().Eq(cql.Int(1)),
-				conditions.Sale.Code.Is().Eq(cql.Int(1)),
-			),
-		)`,
+			_ = %s[models.Product](
+				db,
+				cql.Or[models.Product](
+					conditions.Product.Int.Is().Eq(cql.Int(1)),
+					conditions.Sale.Code.Is().Eq(cql.Int(1)),
+				),
+			)`,
 			Error: `cannot use conditions.Sale.Code.Is().Eq(cql.Int(1)) (value of type condition.WhereCondition[models.Sale]) as condition.WhereCondition[models.Product] value in argument to cql.Or[models.Product]: condition.WhereCondition[models.Sale] does not implement condition.WhereCondition[models.Product] (wrong type for method interfaceVerificationMethod)`,
 		},
 		{
 			Name: "Use condition of another model inside slice operator",
 			Code: `
-		_ = %s[models.Company](
-			db,
-			conditions.Company.Sellers.Any(
-				conditions.Sale.Code.Is().Eq(cql.Int(1)),
-			),
-		)`,
+			_ = %s[models.Company](
+				db,
+				conditions.Company.Sellers.Any(
+					conditions.Sale.Code.Is().Eq(cql.Int(1)),
+				),
+			)`,
 			Error: `cannot use conditions.Sale.Code.Is().Eq(cql.Int(1)) (value of type condition.WhereCondition[models.Sale]) as condition.WhereCondition[models.Seller] value in argument to conditions.Company.Sellers.Any: condition.WhereCondition[models.Sale] does not implement condition.WhereCondition[models.Seller] (wrong type for method interfaceVerificationMethod)`,
 		},
 		{
 			Name: "Condition with field of another type",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Is().Eq(conditions.Product.ID),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Is().Eq(conditions.Product.ID),
+			)`,
 			Error: `cannot use conditions.Product.ID (variable of type condition.Field[models.Product, model.UUID]) as condition.ValueOfType[float64] value in argument to conditions.Product.Int.Is().Eq: condition.Field[models.Product, model.UUID] does not implement condition.ValueOfType[float64] (wrong type for method GetValue)`,
 		},
 		{
 			Name: "Use operator not present for field type",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Is().True(),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Is().True(),
+			)`,
 			Error: `conditions.Product.Int.Is().True undefined (type condition.NumericFieldIs[models.Product] has no field or method True)`,
 		},
 		{
 			Name: "Use custom operator not present for field type",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Is().Custom(
-				condition.Like("_a!_").Escape('!'),
-			),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Is().Custom(
+					condition.Like("_a!_").Escape('!'),
+				),
+			)`,
 			Error: `cannot use condition.Like("_a!_").Escape('!') (value of type condition.ValueOperator[string]) as condition.Operator[float64] value in argument to conditions.Product.Int.Is().Custom: condition.ValueOperator[string] does not implement condition.Operator[float64] (wrong type for method InterfaceVerificationMethod)`,
 		},
 		{
 			Name: "Use function not present for field type",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Concat("asd").Is().Eq(cql.Int(1)),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Concat("asd").Is().Eq(cql.Int(1)),
+			)`,
 			Error: `conditions.Product.Int.Concat undefined (type condition.NumericField[models.Product, int] has no field or method Concat)`,
 		},
 		{
 			Name: "Use function with incorrect value type",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Plus("asd").Is().Eq(cql.Int(1)),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Plus("asd").Is().Eq(cql.Int(1)),
+			)`,
 			Error: `cannot use "asd" (untyped string constant) as float64 value in argument to conditions.Product.Int.Plus`,
 		},
 		{
 			Name: "Use function not present for field type inside comparison",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Is().Eq(conditions.Product.Int.Concat("asd")),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Is().Eq(conditions.Product.Int.Concat("asd")),
+			)`,
 			Error: `conditions.Product.Int.Concat undefined (type condition.NumericField[models.Product, int] has no field or method Concat)`,
 		},
 		{
 			Name: "Use function with incorrect value type inside comparison",
 			Code: `
-		_ = %s[models.Product](
-			db,
-			conditions.Product.Int.Is().Eq(conditions.Product.Int.Plus("asd")),
-		)`,
+			_ = %s[models.Product](
+				db,
+				conditions.Product.Int.Is().Eq(conditions.Product.Int.Plus("asd")),
+			)`,
 			Error: `cannot use "asd" (untyped string constant) as float64 value in argument to conditions.Product.Int.Plus`,
 		},
-
-		// delete:
-		// mas que necesita al menos una condicion
-
-		// group by aggregaciones que no existen para el tipo de dato
-		// having tests de que el tipo es el correcto (constante y otra agregacion)
-
-		// update
-		// set de algo que nada que ver
-		// set luego de funcion (lo hice hace poco)
-		// set null para tipos no nullables
-		// tambien ver en mas de uno
-		// returning solo del tipo de query
-		// funciones en el eq de set
 	}
 
 	for _, testCase := range tests {
 		for _, method := range queryMethods {
-			t.Run(testCase.Name, func(t *testing.T) {
+			internalTestCase := testCase
+
+			t.Run(method+"_"+internalTestCase.Name, func(t *testing.T) {
 				t.Parallel()
 
-				query := fmt.Sprintf(testCase.Code, method)
-				if method != "cql.Query" {
-					query = "_, " + query
+				internalTestCase.Code = fmt.Sprintf(internalTestCase.Code, method)
+
+				if strings.Contains(internalTestCase.Error, "%s") {
+					internalTestCase.Error = fmt.Sprintf(internalTestCase.Error, method)
 				}
 
-				code := `
+				executeTest(t, internalTestCase)
+			})
+		}
+	}
+}
+
+func executeTest(t *testing.T, testCase testCase) {
+	t.Helper()
+
+	code := `
 package main
 
 import (
@@ -247,30 +247,123 @@ import (
 var db *gorm.DB
 
 func main() {
-` + query + `
+` + testCase.Code + `
 }
 `
 
-				tempDir := t.TempDir()
+	tempDir := t.TempDir()
 
-				f, err := os.CreateTemp(tempDir, "cql-test-*.go")
-				require.NoError(t, err)
+	f, err := os.CreateTemp(tempDir, "cql-test-*.go")
+	require.NoError(t, err)
 
-				// Write data to the temporary file
-				_, err = f.WriteString(code)
-				require.NoError(t, err)
+	// Write data to the temporary file
+	_, err = f.WriteString(code)
+	require.NoError(t, err)
 
-				cmd := exec.Command("go", "build", "-o", f.Name()+".exe", f.Name()) //nolint:gosec // necessary for the test
+	cmd := exec.Command("go", "build", "-o", f.Name()+".exe", f.Name()) //nolint:gosec // necessary for the test
 
-				output, err := cmd.CombinedOutput()
-				require.Error(t, err)
+	output, err := cmd.CombinedOutput()
+	require.Error(t, err)
 
-				if strings.Contains(testCase.Error, "%s") {
-					assert.Contains(t, string(output), fmt.Sprintf(testCase.Error, method))
-				} else {
-					assert.Contains(t, string(output), testCase.Error)
-				}
-			})
-		}
+	assert.Contains(t, string(output), testCase.Error)
+}
+
+func TestDeleteCompilationErrors(t *testing.T) {
+	t.Parallel()
+
+	tests := []testCase{
+		{
+			Name: "without any condition",
+			Code: `
+	_ = cql.Delete[models.Product](
+		db,
+	)`,
+			Error: `not enough arguments in call to cql.Delete[models.Product]`,
+		},
 	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.Name, func(t *testing.T) {
+			t.Parallel()
+
+			executeTest(t, testCase)
+		})
+	}
+}
+
+func TestGroupByCompilationErrors(t *testing.T) {
+	t.Parallel()
+
+	tests := []testCase{
+		{
+			Name: "aggregation do not exist for value type",
+			Code: `
+		_ = cql.Query[models.Product](
+			db,
+		).GroupBy(
+			conditions.Product.Int,
+		).Select(
+			conditions.Product.Int.Aggregate().All(), "aggregation1",
+		)`,
+			Error: `conditions.Product.Int.Aggregate().All undefined (type condition.NumericFieldAggregation has no field or method All)`,
+		},
+		{
+			Name: "having not compared with correct type of value",
+			Code: `
+		_ = cql.Query[models.Product](
+			db,
+		).GroupBy(
+			conditions.Product.Int,
+		).Having(
+			conditions.Product.Int.Aggregate().Max().Eq(cql.String("13")),
+		).Select(
+			conditions.Product.Int.Aggregate().Max(), "aggregation1",
+		)`,
+			Error: `cannot use cql.String("13") (value of type condition.Value[string]) as condition.ValueOfType[float64] value in argument to conditions.Product.Int.Aggregate().Max().Eq: condition.Value[string] does not implement condition.ValueOfType[float64] (wrong type for method GetValue)`,
+		},
+		{
+			Name: "having not compared with correct type of another aggregation",
+			Code: `
+		_ = cql.Query[models.Product](
+			db,
+		).GroupBy(
+			conditions.Product.Int,
+		).Having(
+			conditions.Product.Int.Aggregate().Max().Eq(conditions.Product.String.Aggregate().Min()),
+		).Select(
+			conditions.Product.Int.Aggregate().Max(), "aggregation1",
+		)`,
+			Error: ` cannot use conditions.Product.String.Aggregate().Min() (value of type condition.AggregationResult[string]) as condition.ValueOfType[float64] value in argument to conditions.Product.Int.Aggregate().Max().Eq: condition.AggregationResult[string] does not implement condition.ValueOfType[float64] (wrong type for method GetValue)`,
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.Name, func(t *testing.T) {
+			t.Parallel()
+
+			executeTest(t, testCase)
+		})
+	}
+}
+
+func TestUpdateCompilationErrors(t *testing.T) {
+	t.Parallel()
+
+	tests := []testCase{}
+
+	for _, testCase := range tests {
+		t.Run(testCase.Name, func(t *testing.T) {
+			t.Parallel()
+
+			executeTest(t, testCase)
+		})
+	}
+
+	// update
+	// set de algo que nada que ver
+	// set luego de funcion (lo hice hace poco)
+	// set null para tipos no nullables
+	// tambien ver en mas de uno
+	// returning solo del tipo de query
+	// funciones en el eq de set
 }
