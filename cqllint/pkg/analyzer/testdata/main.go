1d66b948b5281f4004df778fb71a114f94e07b91
FrancoLiberali <franco.liberali@gmail.com>
2025-08-24 21:34:47 -0300

    update depencies 2

---
+++ cqllint/pkg/analyzer/testdata/main.go
@@ -0,0 +1,12 @@
+package main
+
+// uncomment to run go mod tidy
+// import (
+// 	_ "appearance"
+// 	_ "not_concerned"
+// 	_ "repeated"
+
+// 	_ "github.com/FrancoLiberali/cql"
+// )
+
+func main() {}
